8df571132719a12adf35984a9771e300b29aa792
Annette <annette.ngugi@student.moringaschool.com>
2025-07-30 18:56:46 +0300

    updated files

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os
+from dotenv import load_dotenv
 from app import create_app
 
-app = create_app()
+
+load_dotenv()
+
+
+config_name = os.getenv('FLASK_CONFIG', 'development')
+
+
+app = create_app(config_name)
+
+
+from seed import seed_command
+app.cli.add_command(seed_command)
 
 if __name__ == '__main__':
     
-    port = int(os.environ.get('PORT', 5000))
-    
-    # Run the application
-    app.run(
-        host='0.0.0.0',
-        port=port,
-        debug=os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
-    )
+    app.run(debug=True)
+
